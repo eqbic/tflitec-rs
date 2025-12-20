61ee70c244a3f64ffef69f55752a612652661a5e
qbic <erichquerner@posteo.org>
2025-12-20 17:20:42 +0100

    update bindgen and use new api

--- build.rs
+++ build.rs
@@ -384,7 +384,7 @@ fn generate_bindings(tf_src_path: PathBuf) {
         .clang_arg(format!("-I{}", tf_src_path.to_str().unwrap()))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
